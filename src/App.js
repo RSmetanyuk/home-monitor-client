b6a8964d467a90783368afe5f9eaa54270535067
RSmetanyuk <rsmetanyuk@gmail.com>
2019-06-18 19:41:27 +0300

    starter react project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+TimeAgo.addLocale(en);
+const timeAgo = new TimeAgo("en-US");
+
+class App extends Component {
+  state = {
+    teperature: [{ time: 1560869528068, value: 26.6 }]
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="centered">
+          Temp: <b>{this.state.teperature[0].value}</b> {"\xB0"}C
+        </div>
+        <div className="centered">
+          <small>{timeAgo.format(this.state.teperature[0].time)}</small>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
