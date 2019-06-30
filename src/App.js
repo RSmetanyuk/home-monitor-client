31fda3b8e5a237a56442d4860df9e3cab3bf491e
Yevhen Motrych <i10111pn@gmail.com>
2019-07-01 02:13:51 +0300

    Readme.md: how to run project locally

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from "react"; //TODO: how this import works?
 
+import "./App.css";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
-TimeAgo.addLocale(en);
-const timeAgo = new TimeAgo("en-US");
 
 class App extends Component {
   state = {
-    teperature: [{ time: 1560869528068, value: 26.6 }]
+    teperature: [{
+      time: 1561934130042,
+      value: 26.6
+    }]
   };
+
+  constructor() {
+    super();
+    TimeAgo.addLocale(en);
+    this.timeAgo = new TimeAgo("en-US");
+  }
 
   render() {
     return (
@@ -17,8 +24,9 @@ class App extends Component {
         <div className="centered">
           Temp: <b>{this.state.teperature[0].value}</b> {"\xB0"}C
         </div>
+
         <div className="centered">
-          <small>{timeAgo.format(this.state.teperature[0].time)}</small>
+          <small>{this.timeAgo.format(this.state.teperature[0].time)}</small>
         </div>
       </React.Fragment>
     );
